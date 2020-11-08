aa1125af36151503f74d7a022955cdb8ce3cdebd
tm33100 <tuuli.matikainen@gmail.com>
2020-11-08 10:13:56 +0200

    Eka commit

--- src/main/java/YhdestaParametriin.java
+++ src/main/java/YhdestaParametriin.java
@@ -5,7 +5,16 @@ public class YhdestaParametriin {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-
+        tulostaLukuunAsti(7);
     }
 
+    public static void tulostaLukuunAsti(int annettuLuku) {
+        int lkm = 0;
+        int alku = 1;
+        int loppu = annettuLuku;
+        while (lkm < loppu) {
+            System.out.println(alku + lkm);
+            lkm++;
+        }
+    }
 }
